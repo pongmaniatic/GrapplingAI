fc374f59750926772a795fb243e820f54613527f
pongmaniatic <37563473+pongmaniatic@users.noreply.github.com>
2021-04-20 15:54:07 +0200

    new project

---
+++ Source/GrapplingAI/GrapplingAIGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GrapplingAIGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AGrapplingAIGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGrapplingAIGameMode();
+};
+
+
+
